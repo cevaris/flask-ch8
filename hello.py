3971df53cbbb60162aa244591c7ca842c008341a
Cevaris <cevaris@gmail.com>
2014-03-09 19:34:51 -0600

    added skills page

--- hello.py
+++ hello.py
@@ -7,8 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-  return render_template('home.html', title="Home Page", header="Hello World!!!")
+  return render_template('home.html', title="Home Page", header="Adam Cardenas")
 
 @app.route("/about")
 def about():
   return render_template('about.html', title="About Me", header="About Me")
+
+@app.route("/skills")
+def skills():
+  return render_template('skills.html', title="Skills", header="Skills")
