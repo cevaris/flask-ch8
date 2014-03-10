70da2bb9ec245e8a39e4020a2511b9b0cfa4423f
Cevaris <cevaris@gmail.com>
2014-03-09 19:29:51 -0600

    added templates; favicon

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
+from flask import render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+  return render_template('home.html', title="Home Page", header="Hello World!!!")
+
+@app.route("/about")
+def about():
+  return render_template('about.html', title="About Me", header="About Me")
